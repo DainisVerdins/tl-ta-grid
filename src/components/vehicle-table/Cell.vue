b26eea79621b1fe8eb0e83272d74f5009fe5ae43
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2023-06-12 20:10:40 +0300

    feat: cell component first iteration

---
+++ src/components/vehicle-table/Cell.vue
@@ -0,0 +1,49 @@
+<template>
+    <div
+        class="car-cell container"
+        @click="remove"
+    >
+        <div class="position-absolute top-50 start-50 translate-middle">
+            <p>{{ vehicle.vehicleNumber }}</p>
+        </div>
+    </div>
+</template>
+    
+<script lang="ts">
+import { PropType, defineComponent } from 'vue'
+import { AssignedVehicle } from '@src/interfaces/assigned-vehicle';
+  
+export default defineComponent({
+    emits: ['vehicle'],
+    props: {
+        vehicle:{
+            required: true,
+            type: Object as PropType<AssignedVehicle>,
+        },
+    },
+    methods: {
+        remove(): void {
+            this.$emit('vehicle', this.vehicle);
+        }
+    },
+})
+  </script>
+    
+  <style scoped lang="scss">
+  .car-cell {
+    width: 100px;
+    height: 50px;
+    border: 1px solid green;
+    position: relative;
+    p {
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+
+    &:hover {
+        background-color: yellow;
+        cursor: pointer;
+    }
+  }
+  </style>
+    
